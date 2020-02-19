414887c910cf9dc2230860e17f74cc81322de599
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2020-02-19 20:26:19 -0300

    added challenges and more folders

---
+++ challenges/input_color.py
@@ -0,0 +1,3 @@
+name = input('Whats is your name? ')
+color = input('Whats is your favorite color? ')
+print(name.title() + ' likes ' + color)
---
+++ challenges/weight.py
@@ -0,0 +1,4 @@
+weight_user = input('Please enter you weight in pounds ' )
+kilograms = int(weight_user)/2.205
+kilograms = str(kilograms)
+print('Your weight in kilograms is ' + kilograms)
